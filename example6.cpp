e44826144babd3397afd73e3986db01b5898a9a8
spror <wiktorspringer27@gmail.com>
2022-03-30 22:57:15 +0200

    addition of a solution to the sixth task

--- example6.cpp
+++ example6.cpp
@@ -6,8 +6,7 @@ using ShoppingList = std::shared_ptr<std::FILE>;
 
 ShoppingList makeFile(const char* filename, const char* flags)
 {
-    /*Edit here*/
-    return std::shared_ptr<std::FILE>();
+    return std::shared_ptr<std::FILE>(fopen(filename, flags), std::fclose);
 }
 
 class Partner
@@ -15,9 +14,14 @@ class Partner
 public:
     void addToFile(std::string element)
     {
-        /*Edit here*/
+        if(!(shoppingList == nullptr))
+        {
+            fprintf(this->shoppingList.get(),"%s \n", element.c_str());
+        }
+        else exit(-1);
     }
     ShoppingList shoppingList;
+
 };
 
 int main()
@@ -29,4 +33,5 @@ int main()
     girl.shoppingList = boy.shoppingList;
     girl.addToFile("Butter");
     girl.addToFile("Milk");
+    
 }
